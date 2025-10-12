00fe4467b3b12214b03d3d00c65980315a19e70d
ovo241089 <OVO241089@GMAIL.COM>
2025-10-12 13:26:39 +0300

    Update Header.tsx

--- src/components/landing/Header.tsx
+++ src/components/landing/Header.tsx
@@ -23,17 +23,7 @@ const Header = () => {
   useMotionValueEvent(scrollY, "change", (latest) => {
     setScrolled(latest > 600);
   });
-  
-<!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=AW-17645502549"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
 
-  gtag('config', 'AW-17645502549');
-</script>
-  
   // Handle logout
   const handleLogout = () => {
     signOut();
